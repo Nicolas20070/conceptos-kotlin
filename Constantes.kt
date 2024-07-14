f26b24913f09574496d9923f036cef238bba2346
Nicolas20070 <159846151+Nicolas20070@users.noreply.github.com>
2024-07-14 09:06:07 -0500

    Update Constantes.kt

--- Constantes.kt
+++ Constantes.kt
@@ -1,11 +1,3 @@
-En Kotlin, las constantes se definen utilizando la palabra clave val. 
-Una constante es un valor que no puede ser cambiado una vez que se le 
-asigna un valor inicial. Esto proporciona inmutabilidad, lo cual es útil 
-cuando queremos asegurarnos de que un valor no cambie durante la ejecución 
-del programa.
-
-EJEMPLO:
-
 fun main() {
     val PI = 3.14159
     val SALUDO = "Hola, mundo!"
